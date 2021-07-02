496a67d3f19f6b57114e6660dc4a1c004889e112
Kevin Funez <kevin.funez315@gmail.com>
2021-07-02 10:05:46 -0700

    add exhange to html

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import RateService from "./js/exchange";
 
-function rateMe(response) {
-  $('#result').append(`<p>Current Exhcange Rates:<span id="exrate">${response.conversion_rates} `);
+function rate(response) {
+  if (response) {
+    $('#result').append(`<p>Current Exhcange Rates${response.conversion_rates.AED}</p>`);
+  }
 }
 
 
@@ -16,10 +18,10 @@ function rateMe(response) {
 
 
 $(document).ready(function() {
-  $('rate').click(function() {
-    RateService.rateMe()
+  $('#rate').click(function() {
+    RateService.getRate()
       .then(function(response) {
-        rateMe(response);
+        rate(response);
       })
       .catch(function(error){
         $('#result').append(`<p>There's been an error and this is it: ${error}<p>`);
